0358df80a678454db470e08dae994a3df4afb5e5
Rodrigo Zamora <rodrigojr907@gmail.com>
2022-09-28 22:05:51 -0300

    Added base project
    Added the base of the project to start learn OOP in Java. Enjoy!

---
+++ Oriented Object Programming/src/java/org/example/entities/Employee.java
@@ -0,0 +1,19 @@
+package org.example.entities;
+
+public class Employee {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary(){
+        return this.grossSalary - this.tax;
+    }
+
+    public void increaseSalary(double percentage){
+        grossSalary += grossSalary * percentage /100;
+    }
+
+    public String toString(){
+        return name + ", $" + String.format("%.2f", netSalary());
+    }
+}
